45e0d800c49a7f038734103c1644ce9abaf02e0b
Daniel Silva de Souza <danielss2990@gmail.com>
2022-01-14 21:59:05 -0300

    axios.get().then((response) => {

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,15 +1,24 @@
+import { useState, useEffect } from "react";
+import { MoviePage } from "types/movie";
 import axios from "axios";
 import { BASE_URL } from "utils/requests";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
-  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
-    console.log(response.data);
-  });
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+      const data = response.data as MoviePage;
+      console.log(data);
+      setPageNumber(data.number);
+    });
+  }, []);
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
